00b19d93de5c2b9da485f474f4a6fc27df56e066
Wajeeh Misbah Khan <wajeeh@Wajeehs-MacBook-Pro.local>
2019-07-17 13:02:20 +0500

    Made connection on index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const { entryType } = require("./Add_Entry_Type/routes");
 const { project } = require("./Add_Projects/routes");
 const { user } = require("./User/user");
 const { nonProfit } = require("./Add_NonProfit/routes");
+const http = require("http");
 
 const app = express();
 require('dotenv').config()
@@ -35,7 +36,27 @@ app.use(bodyParser.json());
 app.use(passport.initialize())
 require("./config/passport")(passport);
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // ROUTES
 app.use("/bank", bank);
@@ -46,6 +67,18 @@ app.use("/projects", project); // Semantic change
 app.use("/non-profit", nonProfit); // New added
 app.use("/user-management", user); // User
 
-app.listen(PORT, hostname, () => {
+const server = http.createServer(app).listen(PORT, hostname, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+const socket = require('socket.io')
+const io = socket.listen(server, {
+  log: false,
+  agent: false,
+  origins: '*:*',
+  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+});
+
+io.on('connection', console.log)
+
+// module.exports.socket = socket;
+// module.exports.io = io;
