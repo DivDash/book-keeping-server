7c3da839e3057bb255f868cac6016fbfd13f5e43
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-08-02 00:56:27 +0500

    Changed schema of journal entry

--- Journal_Entries/schema.js
+++ Journal_Entries/schema.js
@@ -3,19 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JournalEntry = new Schema({
-    particulars: String,
     costCenterId: String,
-    // Not needed
-    // currentBalance : Number,
-    receivingAccountId: String,
-    sendingAccountId: String,
-    transferredAmount: Number,
-    // creditedAmount: Number,
-    // debitedAmount: Number,
+    debitAccountId: String,
+    creditAccountId: String,
+    amount: Number,
     typeOfEntry: String,
-    date: Date,
-    // Not needed
-    // status: String,
+    particulars: String,
+    date: Date
 })
 
 module.exports = mongoose.model('JournalEntry', JournalEntry);
